a94fb7ceb52f5136a9b4dce2a799a70674d21076
asier-acuna <asier.acuna@rai.usc.es>
2025-03-11 14:30:45 +0100

    tocando padding de nuevo

--- app/src/main/java/com/example/impacthon/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/impacthon/ui/map/MapFragment.kt
@@ -40,8 +40,8 @@ class MapFragment : Fragment() {
                             center(Point.fromLngLat( -5.5, 28.8))
                             pitch(0.0)
                             bearing(0.0)
-                            // Añadir paddin en el top
-                            padding(EdgeInsets(1000.0, 0.0, 0.0, 0.0))
+                            // Añadir padding en el top
+                            padding(EdgeInsets(1200.0, 0.0, 0.0, 0.0))
                         }
                     }
                 )
